bdf93221940dc9a9f3e97e3b613e714a3fa6770a
Eugene Lee <eugene@ugenbuch.home>
2014-07-23 17:50:17 -0400

    Modified menu controller so that clicking the portrait again closes the menu

--- Assets/Scripts/GameController/MenuController.cs
+++ Assets/Scripts/GameController/MenuController.cs
@@ -4,34 +4,47 @@ using System.Collections;
 public class MenuController : MonoBehaviour {
 
 	public GameObject MenuBox;
-
+	private bool hasInstantiated = false;
+	private GameObject equipment;
+	private GameObject abilities;
+	private GameObject inventory;
+	private GameObject status;
+	private Transform equipLabel;
+	private Transform abilitiesLabel;
+	private Transform inventoryLabel;
+	private Transform statusLabel;
+	
 	void OnMouseDown()
 	{
-		GameObject equipment;
-		GameObject abilities;
-		GameObject inventory;
-		GameObject status;
-		Transform equipLabel;
-		Transform abilitiesLabel;
-		Transform inventoryLabel;
-		Transform statusLabel;
+		if (!hasInstantiated)
+		{
+			Vector3 relVector = new Vector3(0.16f, 0, 0);
+			Vector3 basePosition = new Vector3(0.1f, 0.05f, 0);
 
-		Vector3 relVector = new Vector3(0.16f, 0, 0);
-		Vector3 basePosition = new Vector3(0.1f, 0.05f, 0);
+			equipment = Instantiate(MenuBox, basePosition, transform.rotation) as GameObject;
+			abilities = Instantiate(MenuBox, basePosition+relVector, transform.rotation) as GameObject;
+			inventory = Instantiate(MenuBox, basePosition+(2*relVector), transform.rotation) as GameObject;
+			status = Instantiate(MenuBox, basePosition+(3*relVector), transform.rotation) as GameObject;
 
-		equipment = Instantiate(MenuBox, basePosition, transform.rotation) as GameObject;
-		abilities = Instantiate(MenuBox, basePosition+relVector, transform.rotation) as GameObject;
-		inventory = Instantiate(MenuBox, basePosition+(2*relVector), transform.rotation) as GameObject;
-		status = Instantiate(MenuBox, basePosition+(3*relVector), transform.rotation) as GameObject;
+			equipLabel = equipment.transform.FindChild("MenuLabel");
+			abilitiesLabel = abilities.transform.Find("MenuLabel");
+			inventoryLabel = inventory.transform.Find("MenuLabel");
+			statusLabel = status.transform.Find ("MenuLabel");
 
-		equipLabel = equipment.transform.FindChild("MenuLabel");
-		abilitiesLabel = abilities.transform.Find("MenuLabel");
-		inventoryLabel = inventory.transform.Find("MenuLabel");
-		statusLabel = status.transform.Find ("MenuLabel");
+			equipLabel.guiText.text = "Equipment";
+			abilitiesLabel.guiText.text = "Abilities";
+			inventoryLabel.guiText.text = "Inventory";
+			statusLabel.guiText.text = "Status";
 
-		equipLabel.guiText.text = "Equipment";
-		abilitiesLabel.guiText.text = "Abilities";
-		inventoryLabel.guiText.text = "Inventory";
-		statusLabel.guiText.text = "Status";
+			hasInstantiated = true;
+		}
+		else
+		{
+			Destroy(equipment);
+			Destroy(abilities);
+			Destroy(inventory);
+			Destroy(status);
+			hasInstantiated = false;
+		}
 	}
 }
