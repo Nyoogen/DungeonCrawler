425c7550021a7f3cc9ef0017046a273b9f6e4dd7
Eugene Lee <eugene@ugenbuch.home>
2014-07-25 19:54:07 -0400

    Field - Actually forgot to uncomment the Scene fading changes, they're in now

--- Assets/Scripts/GameController/SceneChanger.cs
+++ Assets/Scripts/GameController/SceneChanger.cs
@@ -4,28 +4,28 @@ using System.Collections;
 public class SceneChanger : MonoBehaviour 
 {
 	public float fadeSpeed = 1.5f;
-//	public Color guiTextColor = Color.red;
+	public Color guiTextColor = Color.red;
 	
 	private bool sceneStarting = true;
 	private bool sceneEnding = false;
 
 	private string sceneString;
-//	private GameObject fieldStatus;	// Only for Field scene
+	private GameObject fieldStatus;	// Only for Field scene
 	
 	void Awake ()
 	{
 //		Debug.Log (Application.loadedLevelName);
 		guiTexture.pixelInset = new Rect (0f, 0f, Screen.width, Screen.height);
 
-//		// We want the GUIText in FieldStatus to be fading in from black to red, so we need to set the initial color to black
-//		if (Application.loadedLevelName == "Field")
-//		{
-//			fieldStatus = GameObject.Find("FieldStatus");
-//			foreach(Transform child in fieldStatus.transform)
-//			{
-//				child.guiText.color = Color.black;
-//			}
-//		}
+		// We want the GUIText in FieldStatus to be fading in from black to red, so we need to set the initial color to black
+		if (Application.loadedLevelName == "Field")
+		{
+			fieldStatus = GameObject.Find("FieldStatus");
+			foreach(Transform child in fieldStatus.transform)
+			{
+				child.guiText.color = Color.black;
+			}
+		}
 	}
 	
 	void Update ()
@@ -45,29 +45,29 @@ public class SceneChanger : MonoBehaviour
 	{
 		guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
 
-//		// Because GUIText is a GUI element, the black texture doesn't blanket over them when transitioning, so I'm lerping these colors too
-//		// Here, I'm identifying all of the children of FieldStatus (NameText, HPText, and MPText) and looping through them. The effect is that I'm lerping all three GUIText elements
-//		if (Application.loadedLevelName == "Field")
-//		{
-//			foreach(Transform child in fieldStatus.transform)
-//			{
-//				child.guiText.color = Color.Lerp (child.guiText.color, guiTextColor, fadeSpeed * Time.deltaTime);
-//			}
-//		}
+		// Because GUIText is a GUI element, the black texture doesn't blanket over them when transitioning, so I'm lerping these colors too
+		// Here, I'm identifying all of the children of FieldStatus (NameText, HPText, and MPText) and looping through them. The effect is that I'm lerping all three GUIText elements
+		if (Application.loadedLevelName == "Field")
+		{
+			foreach(Transform child in fieldStatus.transform)
+			{
+				child.guiText.color = Color.Lerp (child.guiText.color, guiTextColor, fadeSpeed * Time.deltaTime);
+			}
+		}
 	}
 	
 	void FadeToBlack ()
 	{
 		guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
 
-//		// See FadeToClear's comment
-//		if (Application.loadedLevelName == "Field")
-//		{
-//			foreach(Transform child in fieldStatus.transform)
-//			{
-//				child.guiText.color = Color.Lerp (child.guiText.color, Color.black, fadeSpeed * Time.deltaTime);
-//			}
-//		}
+		// See FadeToClear's comment
+		if (Application.loadedLevelName == "Field")
+		{
+			foreach(Transform child in fieldStatus.transform)
+			{
+				child.guiText.color = Color.Lerp (child.guiText.color, Color.black, fadeSpeed * Time.deltaTime);
+			}
+		}
 	}
 	
 	public void StartScene ()
