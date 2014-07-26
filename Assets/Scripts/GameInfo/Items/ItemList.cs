17b816c7077a7a66db10943251e016a1701d360e
Eugene Lee <eugene@ugenbuch.home>
2014-07-26 19:54:24 -0400

    - forgot to add item names to the actual items. Did that.

--- Assets/Scripts/GameInfo/Items/ItemList.cs
+++ Assets/Scripts/GameInfo/Items/ItemList.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class ItemList : MonoBehaviour 
 {
-	public Consumable potion = new Consumable(10f, 0);
-	public Consumable ether = new Consumable(0, 10f);
-	public Consumable blindPotion = new Consumable(0, 0, true, new string[1]{"blind"});
+	public Consumable potion = new Consumable("Potion", 10f, 0);
+	public Consumable ether = new Consumable("Ether", 0, 10f);
+	public Consumable blindPotion = new Consumable("Blind Potion", 0, 0, true, new string[1]{"blind"});
 
 	public Equipment woodenSword = new Equipment();
 }
