5c20096df922ced0c074c0aba6842b4bbff1788c
dancerdevin <devinrfitzpatrick@gmail.com>
2014-07-25 21:19:12 -0400

    Town dialogue with images, exit to field
    master coder codermaster

--- Assets/Scripts/GameController/TownController.cs
+++ Assets/Scripts/GameController/TownController.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class TownController : MonoBehaviour 
 {
 	private bool showButtons = true;
+	private bool showDialogue = false;
+	private bool showDialogueStore = false;
+	private bool showDialogueInn = false;
+	public Texture horseHead;
+	public Texture dJdicpic;
 
 	void OnGUI()
 	{
@@ -12,19 +17,47 @@ public class TownController : MonoBehaviour
 			float xShift = 200.0f;
 			if(GUI.Button(new Rect(280, 550, 160, 60), "Haberdasher"))
 			{
-				// Go to equip menu
+				showButtons = false;
+				showDialogue = true;
+				showDialogueStore = true;
 			}
 			
 			if (GUI.Button(new Rect(280+xShift, 550, 160, 60), "Caravansary"))
 			{
-				// Go to abilities menu
+				showButtons = false;
+				showDialogue = true;
+				showDialogueInn = true;
 			}
 			
 			if (GUI.Button (new Rect(280+(2*xShift), 550, 160, 60), "Abyss"))
 			{
-				// Go to items menu
+				Application.LoadLevel("Field");
 			}
 			
 		}
+		if (showDialogueStore)
+		{
+			GUI.Box(new Rect(50, 50, 300, 300), horseHead);
+			GUI.Box(new Rect(350, 50, 100, 100), "Hello, friend.");
+		}
+		if (showDialogueInn)
+		{
+			GUI.Box(new Rect(50, 50, 300, 300), dJdicpic);
+			GUI.Box(new Rect(350, 50, 300, 100), "Broooooooooooooooooo!");
+		}
 	}
+	
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0)) // 0 always means left mouse button. This is hard-coded.
+		{
+			if(showDialogue)
+			{
+				showButtons = true;
+				showDialogue = false;
+				showDialogueStore = false;
+				showDialogueInn = false;
+			}
+		}
+	}		
 }
